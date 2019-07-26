9ff4ee3f1c1ccac12a73d4e783580ce364e4e426
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-26 20:40:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,22 +1,12 @@
 def run_guessing_game
-  # goals: 
-  # gets a random number
-  # gets user_input 
-  # compares random number and user_input
-  # if random number and user input aren't equal ask for user_input again (loop)
-  # when random number and user_input are equal ask for user_input again
-  # when user_input is equal to "exit" break the loop and exit the program
-    random_number = rand(6) + 1
-
-    puts "Guess a number between 1 and 6."
-    user_input = gets.chomp
-#    random_number = rand(1..6).to_s
-  
-   if user_input == random_number.to_s
-      puts "You guessed the correct number!"
-   elsif  
-      puts "Goodbye!"
-   else
+  random_number = rand(6) + 1 # <-- Cannot give it as a range i.e. rand(1..6) as the test will reject it, but using a range is viable code outside of this restriction.
+  puts "Guess a number between 1 and 6."
+  user_input = gets.chomp
+  if user_input == random_number.to_s
+    puts "You guessed the correct number!"
+  elsif  
+    puts "Goodbye!"
+  else
       puts "Sorry! The computer guessed #{random_number}."
-   end
+  end
 end
