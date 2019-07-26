592b6d37b0e899efdc6afb1c8709146b0b948f44
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-26 20:36:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,13 +6,13 @@ def run_guessing_game
   # if random number and user input aren't equal ask for user_input again (loop)
   # when random number and user_input are equal ask for user_input again
   # when user_input is equal to "exit" break the loop and exit the program
-    random_number = rand(1..6).to_s
+    random_number = rand(6) + 1
 
     puts "Guess a number between 1 and 6."
     user_input = gets.chomp
 #    random_number = rand(1..6).to_s
   
-   if user_input == random_number
+   if user_input == random_number.to_s
       puts "You guessed the correct number!"
    elsif  
       puts "Goodbye!"
