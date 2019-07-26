bfb67bcbce63a8d1c67ddec0eba1c953866b6396
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-26 20:33:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,9 +14,9 @@ def run_guessing_game
   
    if user_input == random_number
       puts "You guessed the correct number!"
-  #  elsif user_input == "exit"
-  #    puts "Goodbye!"
-    else
+   elsif  
+      puts "Goodbye!"
+   else
       puts "Sorry! The computer guessed #{random_number}."
-    end
+   end
 end
